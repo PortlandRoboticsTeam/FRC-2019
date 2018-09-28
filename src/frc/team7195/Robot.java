42987516a349e251bb0dff33bef3b5d789b1a3e7
Gerrit-Hoving <333woodsia@gmail.com>
2018-09-27 21:33:53 -0400

    Initial commit

---
+++ src/frc/team7195/Robot.java
@@ -0,0 +1,116 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.team7195;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.team7195.commands.AltAutonomus;
+import frc.team7195.commands.BaseAutonomus;
+import frc.team7195.subsystems.Drivetrain;
+import frc.team7195.subsystems.Pnuematics;
+
+public class Robot extends TimedRobot 
+{
+
+    public static final Drivetrain Drivetrain = new Drivetrain();
+    public static final Pnuematics Pnuematics = new Pnuematics();
+    public static OI oi;
+
+    private Command autonomousCommand;
+    private SendableChooser<Command> chooser = new SendableChooser<>();
+
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    @Override
+    public void robotInit() 
+    {
+        oi = new OI();
+        chooser.addDefault("Default Auto", new BaseAutonomus());
+        chooser.addObject("Secondary Auto", new AltAutonomus());
+        SmartDashboard.putData("Auto mode", chooser);
+    }
+
+    /**
+     * This function is called once each time the robot enters Disabled mode.
+     * You can use it to reset any subsystem information you want to clear when
+     * the robot is disabled.
+     */
+    @Override
+    public void disabledInit() 
+    {
+        
+    }
+
+    @Override
+    public void disabledPeriodic() 
+    {
+        Scheduler.getInstance().run();
+    }
+
+    /**
+     * This code helps set up the autonomus by accepting input from the
+     * sendableChooser above.
+     */
+    @Override
+    public void autonomousInit() 
+    {
+        autonomousCommand = chooser.getSelected();
+
+        // Schedules the autonomous command
+        if (autonomousCommand != null) 
+        {
+            autonomousCommand.start();
+        }
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() 
+    {
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void teleopInit() 
+    {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null) 
+        {
+            autonomousCommand.cancel();
+        }
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() 
+    {
+        Scheduler.getInstance().run();
+
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() 
+    {
+        
+    }
+}
