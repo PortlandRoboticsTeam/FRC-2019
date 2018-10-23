0c1554e0e2184e5135a4417a4eaea38a90e1cfe0
Gerrit-Hoving <333woodsia@gmail.com>
2018-10-22 21:18:36 -0400

    Finished documentation, should be done for 2019 season

--- src/frc/team7195/commands/BaseAutonomus.java
+++ src/frc/team7195/commands/BaseAutonomus.java
@@ -5,8 +5,7 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class BaseAutonomus extends Command {
     public BaseAutonomus() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+
     }
 
 
@@ -21,7 +20,7 @@ public class BaseAutonomus extends Command {
 
 
     /**
-     * The execute method is called repeatedly when this Command is
+     * The execute method is called repeatedly while this Command is
      * scheduled to run until this Command either finishes or is canceled.
      */
     @Override
@@ -31,34 +30,20 @@ public class BaseAutonomus extends Command {
 
 
     /**
-     * <p>
-     * Returns whether this command is finished. If it is, then the command will be removed and
-     * {@link #end()} will be called.
-     * </p><p>
-     * It may be useful for a team to reference the {@link #isTimedOut()}
-     * method for time-sensitive commands.
-     * </p><p>
-     * Returning false will result in the command never ending automatically. It may still be
-     * cancelled manually or interrupted by another command. Returning true will result in the
-     * command executing once and finishing immediately. It is recommended to use
-     * {@link edu.wpi.first.wpilibj.command.InstantCommand} (added in 2017) for this.
-     * </p>
      *
-     * @return whether this command is finished.
-     * @see Command#isTimedOut() isTimedOut()
+     * Returns whether this command is finished. If it is, then the command will be removed and
+     * end() will be called.
+     * If this class returns true, the command finishes, if it returns false, the command will continue.
      */
     @Override
     protected boolean isFinished() {
-        // TODO: Make this return true when this Command no longer needs to run execute()
         return false;
     }
 
 
     /**
      * Called once when the command ended peacefully; that is it is called once
-     * after {@link #isFinished()} returns true. This is where you may want to
-     * wrap up loose ends, like shutting off a motor that was being used in the
-     * command.
+     * after isFinished() returns true. Used to wrap up loose ends.
      */
     @Override
     protected void end() {
@@ -67,21 +52,14 @@ public class BaseAutonomus extends Command {
 
 
     /**
-     * <p>
-     * Called when the command ends because somebody called {@link #cancel()} or
+     * Called when the command ends because somebody called cancel() or
      * another command shared the same requirements as this one, and booted it out. For example,
      * it is called when another command which requires one or more of the same
-     * subsystems is scheduled to run.
-     * </p><p>
-     * This is where you may want to wrap up loose ends, like shutting off a motor that was being
-     * used in the command.
-     * </p><p>
-     * Generally, it is useful to simply call the {@link #end()} method within this
-     * method, as done here.
-     * </p>
+     * subsystems is scheduled to run. Generally, it is useful to simply call the
+     * end() method here
      */
     @Override
     protected void interrupted() {
-        super.interrupted();
+        super.end();
     }
 }
--- src/frc/team7195/subsystems/Drivetrain.java
+++ src/frc/team7195/subsystems/Drivetrain.java
@@ -6,9 +6,11 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Drivetrain extends Subsystem {
 
     /**
+     * Subsystems are used to group hardware and define ways it is used.
+     *
      * In this class you declare the motor controllers and related
-     * hardware and define the way to use the system. For example, different
-     * joystick steering methods and autonomous driving code goes here.
+     * hardware and define the ways this system is used. For example, different
+     * joystick driving methods and autonomous driving code goes here.
      **/
 
 
