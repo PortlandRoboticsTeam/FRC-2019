43427d9d1a8a98e7239954c467713d794e13ef9f
Gerrit-Hoving <333woodsia@gmail.com>
2019-01-14 22:14:08 -0500

    Cleaned up drivetrain

--- src/frc/team7195/subsystems/Drivetrain.java
+++ src/frc/team7195/subsystems/Drivetrain.java
@@ -1,14 +1,12 @@
 package frc.team7195.subsystems;
 
 
-import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import frc.team7195.OI;
 
-import static frc.team7195.Robot.oi;
 
 public class Drivetrain extends Subsystem {
 
@@ -20,33 +18,22 @@ public class Drivetrain extends Subsystem {
      * joystick driving methods and autonomous driving code goes here.
      **/
 
-    static Spark frontRight = new Spark(1);
-    static Spark rearRight = new Spark(2);
-    static Spark frontLeft = new Spark(3);
-    static Spark rearLeft = new Spark(4);
+    private static Spark frontRight = new Spark(1);
+    private static Spark rearRight = new Spark(2);
+    private static Spark frontLeft = new Spark(3);
+    private static Spark rearLeft = new Spark(4);
     //static AnalogGyro m_gyro = new AnalogGyro(1);
 
-    static SpeedControllerGroup right = new SpeedControllerGroup(frontRight, rearRight);
-    static SpeedControllerGroup left = new SpeedControllerGroup(frontLeft, rearLeft);
+    private static SpeedControllerGroup right = new SpeedControllerGroup(frontRight, rearRight);
+    private static SpeedControllerGroup left = new SpeedControllerGroup(frontLeft, rearLeft);
 
-    public static MecanumDrive mechanumDrive = new MecanumDrive(frontRight, rearRight, frontLeft, rearLeft);
-    public static DifferentialDrive drive = new DifferentialDrive(right, left);
+    private static DifferentialDrive drive = new DifferentialDrive(right, left);
 
-    public static void tankDrive() {
-        drive.tankDrive(oi.m_stick.getX(), oi.s_stick.getX());
-    }
+    public static void tankDrive() { drive.tankDrive(OI.m_stick.getX(), OI.s_stick.getX()); }
 
-    public static void curvatureDrive() {
-        drive.curvatureDrive(oi.m_stick.getX(), oi.m_stick.getZ(), true);
-    }
+    public static void curvatureDrive() { drive.curvatureDrive(OI.m_stick.getX(), OI.m_stick.getZ(), true); }
 
-    public static void standardDrive() {
-        drive.arcadeDrive(oi.m_stick.getX(), oi.m_stick.getY());
-    }
-
-    public static void mecanumDrive() {
-        mechanumDrive.driveCartesian(oi.m_stick.getX(), oi.m_stick.getY(), oi.m_stick.getZ()/*, m_gyro.getAngle()*/);
-    }
+    public static void standardDrive() { drive.arcadeDrive(OI.m_stick.getX(), OI.m_stick.getY()); }
 
     public void initDefaultCommand() {
         //setDefaultCommand(new ExampleCommand());
