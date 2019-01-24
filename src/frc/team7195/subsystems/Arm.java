f70affc0bd0b1c9d77d6e6d2cda210d0de7b2876
Gerrit-Hoving <333woodsia@gmail.com>
2019-01-24 15:09:57 -0500

    Set up arm and elevator subsystems

---
+++ src/frc/team7195/subsystems/Arm.java
@@ -0,0 +1,28 @@
+package frc.team7195.subsystems;
+
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Arm extends Subsystem {
+
+    public static DoubleSolenoid armValve = new DoubleSolenoid(5, 6);
+
+    public static void solenoidPositionOne() {
+        armValve.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public static void solenoidPositionTwo() {
+        armValve.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public static void solenoidPositionOff() {
+        armValve.set(DoubleSolenoid.Value.kOff);
+    }
+
+    public void initDefaultCommand() {
+        // TODO: Set the default command, if any, for a subsystem here. Example:
+        //    setDefaultCommand(new MySpecialCommand());
+    }
+}
+
---
+++ src/frc/team7195/subsystems/Elevator.java
@@ -0,0 +1,24 @@
+package frc.team7195.subsystems;
+
+
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Elevator extends Subsystem {
+
+    public static PWMTalonSRX elevatorMotor = new PWMTalonSRX(0);
+
+    public static void elevatorUp() {
+        elevatorMotor.set(0);
+    }
+
+    public static void elevatorDown() {
+        elevatorMotor.set(1);
+    }
+
+    public void initDefaultCommand() {
+        // TODO: Set the default command, if any, for a subsystem here. Example:
+        //    setDefaultCommand(new MySpecialCommand());
+    }
+}
+
